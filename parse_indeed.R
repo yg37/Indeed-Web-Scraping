67e2393c0cd87aa486be8e08ce8b47f58c28658f
Fu Wen <fu.wen@duke.edu>
2015-12-03 20:13:01 -0500

    Debug it

--- parse_indeed.R
+++ parse_indeed.R
@@ -19,7 +19,8 @@ for (i in 1:length(files)){
 
 htmls = dir("data/links",pattern = "*.html",full.names = TRUE)
 
-job_summary <- rep(NA, length(htmls))
+job_summary <- NULL
+num <- NULL
 
 for (i in 1:length(htmls))
 {
@@ -27,43 +28,52 @@ for (i in 1:length(htmls))
   
   content <- html_nodes(html, "#job_summary") %>% html_text()
   
-  job_summary[i] <- ifelse(length(content),tolower(content),NA)
+  if (length(content)==0) next
   
+  content <- str_replace_all(tolower(content),"[^a-z0-9[:punct:] ]", " ")
+  
+  job_summary<- c(job_summary,content)
+  
+  num <- c(num,i)
 }
 
-qualifications <- rep(NA, length(htmls))
-responsibilities <- rep(NA, length(htmls))
-q1 <- rep(NA, length(htmls))
-q2 <- rep(NA, length(htmls))
-q3 <- rep(NA, length(htmls))
-q4 <- rep(NA, length(htmls))
-q5 <- rep(NA, length(htmls))
-q6 <- rep(NA, length(htmls))
-q7 <- rep(NA, length(htmls))
-r1 <- rep(NA, length(htmls))
-r2 <- rep(NA, length(htmls))
-r3 <- rep(NA, length(htmls))
-r4 <- rep(NA, length(htmls))
+
+
+qualifications <- NULL
+responsibilities <- NULL
+q1 <- q2 <- q3 <- q4 <- q5 <- q6 <- q7 <- r1 <- r2 <- r3 <- r4 <- NULL 
 
 for(i in 1:length(job_summary))
 {
-  q1[i] <- str_extract_all(job_summary[i],"qualifications?.*?\\n(\\n|[:blank:])?")
-  q2[i] <- str_extract_all(job_summary[i],"qualified.*?\\n(\\n|[:blank:])?")
-  q3[i] <- str_extract_all(job_summary[i],"requirements?.*?\\n(\\n|[:blank:])?")
-  q4[i] <- str_extract_all(job_summary[i],"demands?.*?\\n(\\n|[:blank:])?")
-  q5[i] <- str_extract_all(job_summary[i],"essentials?.*?\\n(\\n|[:blank:])?")
-  q6[i] <- str_extract_all(job_summary[i],"must?.*?\\n(\\n|[:blank:])?")
-  q7[i] <- str_extract_all(job_summary[i],"should?.*?\\n(\\n|[:blank:])?")
-  qualifications[i] <- str_c(unlist(q1[i]), unlist(q2[i]), unlist(q3[i]), sep = " ")
-  qualifications[i] <- ifelse(str_length(qualifications[i]), 
-                              qualifications[i],
-                              str_c(unlist(q4[i]), unlist(q5[i]), unlist(q6[i]), unlist(q7[i]), sep = " "))
+  q1 <- str_extract_all(job_summary[i],"qualifi[a-z]+[: ]+.+? {2}") %>% unlist()
+  q3 <- str_extract_all(job_summary[i],"require[a-z]+[: ]+.+? {2}") %>% unlist()
+  q4 <- str_extract_all(job_summary[i],"demand[a-z]+[: ]+.+? {2}") %>% unlist()
+  q5 <- str_extract_all(job_summary[i],"essentials[a-z]+[: ]+.+? {2}") %>% unlist()
+  q6 <- str_extract_all(job_summary[i],"must[a-z]+[: ]+.+? {2}") %>% unlist()
+  q7 <- str_extract_all(job_summary[i],"should[a-z]+[: ]+.+? {2}") %>% unlist()
   
-  r1[i] <- str_extract_all(job_summary[i],"responsib.*?\\n(\\n|[:blank:])?")
-  r2[i] <- str_extract_all(job_summary[i],"accountab.*?\\n(\\n|[:blank:])?")
-  r3[i] <- str_extract_all(job_summary[i],"duties.*?\\n(\\n|[:blank:])?")
-  r4[i] <- str_extract_all(job_summary[i],"functions?.*?\\n(\\n|[:blank:])?")
-  responsibilities[i] <- str_c(unlist(r1[i]), unlist(r2[i]), unlist(r3[i]), sep = " ")
-  responsibilities[i] <- ifelse(str_length(responsibilities[i]), responsibilities[i], r4[i])
+  qua<-NULL
+  
+  if (length(q1)!=0|length(q2)!=0|length(q3)!=0)
+  qua <- paste(q1, q2, q3, sep = " ") 
+  
+  if(length(q4)!=0|length(q5)!=0|length(q6)!=0|length(q7)!=0)
+  qua <- ifelse(length(qua),qua,paste(q4, q5, q6, q7, sep = " "))
+  
+  qualifications <- c(qualifications,qua)
+  
+  r1 <- str_extract_all(job_summary[i],"responsi[a-z]+[: ]+.+? {2}") %>% unlist()
+  r2 <- str_extract_all(job_summary[i],"accountab[a-z]+[: ]+.+? {2}") %>% unlist()
+  r3 <- str_extract_all(job_summary[i],"duties[a-z]+[: ]+.+? {2}") %>% unlist()
+  r4 <- str_extract_all(job_summary[i],"function[a-z]+[: ]+.+? {2}") %>% unlist()
+  
+  res <- NULL
+  
+  if (length(r1)!=0|length(r2)!=0|length(r3)!=0)
+  res <- paste(r1, r2, r3, sep = " ")
+  
+  if(length(r4)!=0)
+  res <- ifelse(length(res), res, r4)
+  
+  responsibilities <- c(responsibilities,res)
 }
-
