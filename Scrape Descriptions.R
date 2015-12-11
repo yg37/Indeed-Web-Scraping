b92989f0a9a3c0c46fb31ca2be3780f8ccf65959
Sophie Guo <yg37@duke.edu>
2015-12-10 22:18:33 -0500

    Updated tf-idf

--- Scrape Descriptions.R
+++ Scrape Descriptions.R
@@ -2,9 +2,10 @@ require(stringr)
 require(rvest)
 require(magrittr)
 require(koRpus)
+require(wordcloud)
 require(openNLP)
 require(tm)
-library(wordcloud)
+require(rJava)
 
 
 files <- dir("data/indeed/", pattern = "*.html", full.names = TRUE)
@@ -48,12 +49,30 @@ corp <-tm_map(corp, removeWords, stopwords('english'))
 
 dtm <- as.matrix(DocumentTermMatrix(corp))
 
+
+
+ 
+
+TFIDF
+norm <- dtm/rowSums(dtm) #normalize the matrix
+nonZero <- colSums(norm != 0)
+weight <- log(dim(norm)[2]/nonZero)
+weight_sorted <- sort(weight,decreasing=TRUE)
+names <- names(weight_sorted)
+weight_matrix <- matrix(0,dim(norm)[1],dim(norm)[2])
+for (i in 1:dim(norm)[2]){
+  weight_matrix[,i] <- norm[,i]*weight[i]
+}
+colnames(weight_matrix) <- colnames(norm)
+rownames(weight_matrix) <- rownames(norm)
+
 sent_token_annotator <- Maxent_Sent_Token_Annotator()
 word_token_annotator <- Maxent_Word_Token_Annotator()
-annotations <- annotate(colnames(dtm), list(sent_token_annotator, word_token_annotator))
+annotations <- annotate(colnames(weight_matrix), list(sent_token_annotator, word_token_annotator))
 pos_tag_annotator <- Maxent_POS_Tag_Annotator()
 pos_tag_annotator
-pos <- annotate(colnames(dtm), pos_tag_annotator, annotations)
+pos <- annotate(colnames(weight_matrix), pos_tag_annotator, annotations)
+
 
 non <- c()
 for (i in 2:length(pos)){
@@ -62,23 +81,30 @@ for (i in 2:length(pos)){
   }
 }
 
+#non_verb <- c()
+#for (i in 2:length(pos)){
+#if (pos$features[[i]]$POS != "VBD" ||pos$features[[i]]$POS != "VBG" ){
+#non_verb<- c(non_verb,i-1)
+#}
+#}
 
 irrelevant <- c()
-for (i in 1:dim(dtm)[2]){
-  if (str_detect(colnames(dtm)[i],"job")||str_detect(colnames(dtm)[i],"http")||str_detect(colnames(dtm)[i],"indeed")){
+for (i in 1:dim(weight_matrix)[2]){
+  if (str_detect(colnames(weight_matrix)[i],"job")||str_detect(colnames(weight_matrix)[i],"http")||str_detect(colnames(weight_matrix)[i],"indeed")){
     irrelevant <- c(irrelevant,i)
   }
 }
 
-dtm <- dtm[,-c(non,irrelevant)]
+#weight_matrix <- weight_matrix[,-irrelevant]
+weight_matrix <- weight_matrix[,-c(non,irrelevant)]
+#weight_matrix <-weight_matrix[,-non_verb]
+#foreach(i = 1:dim(weight_matrix)[2]) %do% if (!pos(colnames(weight_matrix)[i])){weight_matrix[,-i]}  
+
 
 
 #Source: http://www.r-bloggers.com/text-mining/#
-v = sort(colSums(dtm), decreasing=TRUE);
+v = sort(colSums(weight_matrix), decreasing=TRUE);
 myNames = names(v);
 d = data.frame(word=myNames, freq=v)
-wordcloud(d$word, d$freq,random.color=T,scale = c(4,.2), 
-          min.freq = 2,max.words=30)
+wordcloud(d$word, colors=brewer.pal(8,"Dark2"), d$freq,max.words=500)
 
-    
-    
